30d0874fb8fa78fb14b14853600606d6c12310c4
Vikrama Dhanvi <p.e.v.dhanvi123@gmail.com>
2025-03-26 22:05:51 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Replace 'your-repo-name' with your actual repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/ToDoLisr-React/' // <-- Important!
+  base: '/ToDoList-React/' // Make sure this matches your GitHub repo name
 })
+
